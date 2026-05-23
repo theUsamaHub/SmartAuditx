d2e2bf2325ba7021928a32040dd89015754ab92a
theUsamaHub <usamabusiness2025@outlook.com>
2026-05-24 01:07:36 +0500

    added the interface for the registerationservice

---
+++ SmartAuditX/Services/Interfaces/IRegistrationService.cs
@@ -0,0 +1,11 @@
+﻿using SmartAuditX.Models.ViewModels;
+
+namespace SmartAuditX.Services.Interfaces
+{
+    public interface IRegistrationService
+    {
+        Task<bool> RegisterCompanyOwnerAsync(
+            RegisterAccountViewModel account,
+            RegisterCompanyViewModel company);
+    }
+}
